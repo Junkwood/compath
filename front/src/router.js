3288c7ffe8a29193d3cbef5efc235a650a6fac74
jingjinge150 <jgh1106@naver.com>
2026-03-24 15:34:12 +0900

    프론트 수정

---
+++ front/src/router.js
@@ -0,0 +1,41 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+/**
+ * 라우터 등록 규칙
+ * - path      : 사이드바 링크와 동일하게 맞출 것
+ * - component : src/pages/ 하위에 파일 생성 후 import
+ * - name      : 카멜케이스로 작성 ex) projectTasks
+ *
+ * [경로 규칙]
+ * 메인            /
+ * 전체 목록       /projects
+ * 프로젝트 하위   /project/:기능명
+ *
+ * [파일 위치 규칙]
+ * 최상위 페이지        → src/pages/페이지명.vue           ex) Dashboard.vue
+ * 프로젝트 하위 페이지  → src/pages/project/페이지명.vue   ex) ProjectTasks.vue
+ */
+
+const routerHistory = createWebHistory();
+
+const router = createRouter({
+  history: routerHistory,
+  routes: [
+    // 예시) 아래 형식으로 추가하기
+    // {
+    //   path: '/project/tasks',
+    //   name: 'projectTasks',
+    //   component: () => import('./pages/project/ProjectTasks.vue'),
+    // },
+  ],
+
+  routes: [
+    {
+      path: "/",
+      name: "dashboard",
+      component: () => import("./pages/Dashboard.vue"),
+    },
+  ],
+});
+
+export default router;
