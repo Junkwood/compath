f61c862233d9313b73a1f7ee41dc65ba48d8d809
jingjinge150 <jgh1106@naver.com>
2026-04-06 11:49:14 +0900

    0406_지원_업무보고서완성본

--- front/src/stores/TaskReport.js
+++ front/src/stores/TaskReport.js
@@ -2,15 +2,55 @@ import { defineStore } from "pinia";
 import api from "../utils/api";
 
 export const useTaskReport = defineStore("TaskReport", {
-  state: () => ({ reportList: [], totalCount: 0 }),
-  getters: {},
+  state: () => ({
+    initTitleList: [], 
+    allData: [],
+    filterList: [],
+    listLength: 0,
+    workPageSize: 0,
+    titleList: [],
+    assigneeList: [],
+    taskTypeList: [],
+  }),
+
   actions: {
-    async fetchReportList(params) {
+    slicePage(page, listNum) {
+      const start = (page - 1) * listNum;
+      const end = start + listNum;
+      this.filterList = this.allData.slice(start, end);
+    },
+
+    async fetchReportList(params, page = 1, listNum = 10,isInit = false) {
       try {
         const response = await api.get("/taskReport", { params });
-        this.reportList = response.data;
-        this.totalCount = response.data.length;
-        return response.data;
+        const data = response.data;
+
+        this.allData = data;
+        this.listLength = data.length;
+        this.workPageSize = data.length;
+        this.slicePage(page, listNum);
+
+         if (isInit) {
+        this.titleList = [...new Set(data.map((t) => t.title).filter(Boolean))];
+
+        this.assigneeList = [
+          ...new Map(
+            data.map((t) => [
+              t.assigneeUserId,
+              { assigneeUserId: t.assigneeUserId, userName: t.userName },
+            ])
+          ).values(),
+        ];
+      
+        this.taskTypeList = [
+          ...new Map(
+            data
+              .filter((t) => t.typeName != null)
+              .map((t) => [t.typeName, { typeName: t.typeName }])
+          ).values(),
+        ];
+      }
+        return data;
       } catch (error) {
         console.error("보고서 로딩 실패:", error);
       }
