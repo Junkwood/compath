2fd77f7726573284d1877ba8289a371941eeef62
Junkwood <156729867+Junkwood@users.noreply.github.com>
2026-04-17 17:16:19 +0900

    0417_서정우_일반계정에서 가능한 roleList로 수정처리

--- front/src/meeting/meetingNotificationModal.vue
+++ front/src/meeting/meetingNotificationModal.vue
@@ -308,8 +308,8 @@ const reset = () => {
 };
 
 onMounted(async () => {
-  await roleStore.getRoleList();
-  roles.value = roleStore.roleList;
+  await roleStore.getActiveRoleList();
+  roles.value = roleStore.activeRoleList;
 });
 </script>
 <style>
